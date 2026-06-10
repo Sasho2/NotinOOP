d132b6f3a00fd1afd491d948de7109e8974d283c
Sasho <i.aleks.kostadinov@gmail.com>
2026-06-10 15:09:43 +0300

    add catalog array and memory management to NotinOOP core

--- NotinOOP.h
+++ NotinOOP.h
@@ -2,19 +2,27 @@
 #include "User.h"
 #include "Buyer.h"
 #include "Admin.h"
+#include "Fragrance.h"
 
 class NotinOOP {
 private:
     User** users;
     int userCount;
     int userCapacity;
+
+    Fragrance** catalog;
+    int catalogCount;
+    int catalogCapacity;
+
     User* loggedInUser;
 
     void resizeUsers();
+    void resizeCatalog();
 
 public:
     NotinOOP();
     ~NotinOOP();
 
     void run();
+    Fragrance* findFragrance(const char* name) const;
 };
--- NotinOOP_Core.cpp
+++ NotinOOP_Core.cpp
@@ -8,9 +8,32 @@ void NotinOOP::resizeUsers() {
     users = temp;
 }
 
-NotinOOP::NotinOOP() : users(nullptr), userCount(0), userCapacity(0), loggedInUser(nullptr) {}
+void NotinOOP::resizeCatalog() {
+    catalogCapacity = (catalogCapacity == 0) ? 2 : catalogCapacity * 2;
+    Fragrance** temp = new Fragrance * [catalogCapacity];
+    for (int i = 0; i < catalogCount; i++) temp[i] = catalog[i];
+    delete[] catalog;
+    catalog = temp;
+}
+
+NotinOOP::NotinOOP() : users(nullptr), userCount(0), userCapacity(0),catalog(nullptr), 
+catalogCount(0), catalogCapacity(0),loggedInUser(nullptr) {}
+
 
 NotinOOP::~NotinOOP() {
     for (int i = 0; i < userCount; i++) delete users[i];
     delete[] users;
+
+    for (int i = 0; i < catalogCount; i++) delete catalog[i];
+    delete[] catalog;
+}
+
+Fragrance* NotinOOP::findFragrance(const char* name) const {
+    String target(name);
+    for (int i = 0; i < catalogCount; i++) {
+        if (catalog[i]->getName() == target) {
+            return catalog[i];
+        }
+    }
+    return nullptr;
 }
